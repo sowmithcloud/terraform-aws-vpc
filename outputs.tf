9f67efe04be8f2b8b9af43db762a754bd4fd1c05
sowmithcloud <sowmithcloud@gmail.com>
2024-04-02 13:03:18 +0530

    resources

--- outputs.tf
+++ outputs.tf
@@ -14,6 +14,6 @@ output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
 
-output "database_subnet_ids" {
-  value = aws_subnet.database[*].id
-}
+# output "database_subnet_ids" {
+#   value = aws_subnet.database[*].id
+# }
